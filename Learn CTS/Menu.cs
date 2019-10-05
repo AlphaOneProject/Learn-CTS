77633367d5aa34347dcd868be4731d7e98eb00fb
Louis Allemann <36743745+jeanlouison@users.noreply.github.com>
2019-10-05 22:38:49 +0200

    Optimisations du changement de menus

--- Learn CTS/Menu.cs
+++ Learn CTS/Menu.cs
@@ -46,7 +46,7 @@ namespace Learn_CTS
         /// <param name="e">Arguments from the action whose caused the call of this method.</param>
         private void Btn_edit_Click(object sender, EventArgs e)
         {
-            Remove_Menu_Controls_By_Tag(displayed_menu);
+            this.Controls.Clear();
             Display_editor_menu();
         }
 
@@ -67,7 +67,7 @@ namespace Learn_CTS
 
         private void Btn_Back_To_Main_Menu_Click(object sender, EventArgs e)
         {
-            Remove_Menu_Controls_By_Tag(displayed_menu);
+            this.Controls.Clear();
             Display_Main_Menu();
         }
 
@@ -85,7 +85,6 @@ namespace Learn_CTS
             btn_play.TabIndex = 0;
             btn_play.Text = "Jouer";
             btn_play.UseVisualStyleBackColor = true;
-            btn_play.Tag = "main_menu";
 
             // 
             // Creation of the button that will show the selection of available levels to edit.
@@ -97,7 +96,6 @@ namespace Learn_CTS
             btn_edit.TabIndex = 1;
             btn_edit.Text = "Editer";
             btn_edit.UseVisualStyleBackColor = true;
-            btn_play.Tag = "main_menu";
             btn_edit.Click += new System.EventHandler(this.Btn_edit_Click);
 
             // 
@@ -110,7 +108,6 @@ namespace Learn_CTS
             btn_exit.TabIndex = 2;
             btn_exit.Text = "Quitter";
             btn_exit.UseVisualStyleBackColor = true;
-            btn_exit.Tag = "main_menu";
             btn_exit.Click += new System.EventHandler(this.Btn_exit_Click);
 
             this.Controls.AddRange(new Control[] { btn_play, btn_edit, btn_exit });
@@ -136,7 +133,6 @@ namespace Learn_CTS
             flp_editor_menu.Size = new Size((int)(this.Width * 0.8), (int)(this.Height * 0.8));
             flp_editor_menu.BackColor = Color.ForestGreen;
             flp_editor_menu.TabIndex = 3;
-            flp_editor_menu.Tag = "editor_menu";
 
             //
             //Creation of the Button responsible to go back to the main menu.
@@ -147,29 +143,12 @@ namespace Learn_CTS
             btn_back_to_main_menu.Size = new Size(150, 23);
             btn_back_to_main_menu.Text = "Retour au menu principal";
             btn_back_to_main_menu.UseVisualStyleBackColor = true;
-            btn_back_to_main_menu.Tag = "editor_menu";
             btn_back_to_main_menu.Click += new EventHandler(this.Btn_Back_To_Main_Menu_Click);
 
             this.Controls.AddRange(new Control[] { flp_editor_menu, btn_back_to_main_menu });
             flp_editor_menu.Controls.Add(btn_back_to_main_menu);
 
             this.ResumeLayout();
-        }
-
-        /// <summary>
-        /// Removes every controls with the parameter tag from the form.
-        /// </summary>
-        /// <param name="ctag">Tag of the controls to remove</param>
-        private void Remove_Menu_Controls_By_Tag(String ctag)
-        {
-            //MessageBox.Show(ctag);
-            foreach(Control c in this.Controls)
-            {
-                if (ctag.Equals((String)c.Tag))
-                {
-                    this.Controls.Remove(c);
-                }
-            }
         }
 
         private void Menu_SizeChanged(object sender, EventArgs e)
