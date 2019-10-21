412db49a03616f6911faa73750d3a313fc2bdd24
Louis Allemann <36743745+jeanlouison@users.noreply.github.com>
2019-10-21 21:22:12 +0200

    Added pretty buttons to game selection cards.

--- Learn CTS/GameCard.Designer.cs
+++ Learn CTS/GameCard.Designer.cs
@@ -29,50 +29,80 @@
         private void InitializeComponent()
         {
             this.lbl_title = new System.Windows.Forms.Label();
-            this.btn_edit = new System.Windows.Forms.Button();
             this.lbl_description = new System.Windows.Forms.Label();
+            this.pb_edit = new System.Windows.Forms.PictureBox();
+            this.pb_play = new System.Windows.Forms.PictureBox();
+            this.pb_thumbnail = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_edit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_play)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_thumbnail)).BeginInit();
             this.SuspendLayout();
             // 
             // lbl_title
             // 
             this.lbl_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_title.Location = new System.Drawing.Point(123, 18);
+            this.lbl_title.Location = new System.Drawing.Point(142, 6);
             this.lbl_title.Name = "lbl_title";
-            this.lbl_title.Size = new System.Drawing.Size(176, 28);
+            this.lbl_title.Size = new System.Drawing.Size(158, 28);
             this.lbl_title.TabIndex = 0;
             this.lbl_title.Text = "Titre";
             // 
-            // btn_edit
-            // 
-            this.btn_edit.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btn_edit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_edit.Location = new System.Drawing.Point(122, 49);
-            this.btn_edit.Name = "btn_edit";
-            this.btn_edit.Size = new System.Drawing.Size(177, 39);
-            this.btn_edit.TabIndex = 1;
-            this.btn_edit.Text = "Editer";
-            this.btn_edit.UseVisualStyleBackColor = false;
-            this.btn_edit.Click += new System.EventHandler(this.Btn_edit_Click);
-            // 
             // lbl_description
             // 
-            this.lbl_description.Location = new System.Drawing.Point(124, 95);
+            this.lbl_description.Location = new System.Drawing.Point(142, 34);
             this.lbl_description.Name = "lbl_description";
-            this.lbl_description.Size = new System.Drawing.Size(179, 38);
+            this.lbl_description.Size = new System.Drawing.Size(158, 100);
             this.lbl_description.TabIndex = 2;
             this.lbl_description.Text = "Description";
+            // 
+            // pb_edit
+            // 
+            this.pb_edit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_edit.Location = new System.Drawing.Point(95, 7);
+            this.pb_edit.Name = "pb_edit";
+            this.pb_edit.Size = new System.Drawing.Size(42, 42);
+            this.pb_edit.TabIndex = 5;
+            this.pb_edit.TabStop = false;
+            this.pb_edit.Click += new System.EventHandler(this.Pb_edit_Click);
+            this.pb_edit.MouseLeave += new System.EventHandler(this.Pb_edit_MouseLeave);
+            this.pb_edit.MouseHover += new System.EventHandler(this.Pb_edit_MouseHover);
+            // 
+            // pb_play
+            // 
+            this.pb_play.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_play.Location = new System.Drawing.Point(28, 28);
+            this.pb_play.Name = "pb_play";
+            this.pb_play.Size = new System.Drawing.Size(86, 86);
+            this.pb_play.TabIndex = 4;
+            this.pb_play.TabStop = false;
+            this.pb_play.Click += new System.EventHandler(this.Pb_play_Click);
+            this.pb_play.MouseLeave += new System.EventHandler(this.Pb_play_MouseLeave);
+            this.pb_play.MouseHover += new System.EventHandler(this.Pb_play_MouseHover);
+            // 
+            // pb_thumbnail
+            // 
+            this.pb_thumbnail.Location = new System.Drawing.Point(7, 7);
+            this.pb_thumbnail.Name = "pb_thumbnail";
+            this.pb_thumbnail.Size = new System.Drawing.Size(128, 128);
+            this.pb_thumbnail.TabIndex = 3;
+            this.pb_thumbnail.TabStop = false;
             // 
             // GameCard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Controls.Add(this.pb_edit);
+            this.Controls.Add(this.pb_play);
+            this.Controls.Add(this.pb_thumbnail);
             this.Controls.Add(this.lbl_description);
-            this.Controls.Add(this.btn_edit);
             this.Controls.Add(this.lbl_title);
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "GameCard";
-            this.Size = new System.Drawing.Size(314, 146);
+            this.Size = new System.Drawing.Size(314, 140);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_edit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_play)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_thumbnail)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -80,7 +110,9 @@
         #endregion
 
         private System.Windows.Forms.Label lbl_title;
-        private System.Windows.Forms.Button btn_edit;
         private System.Windows.Forms.Label lbl_description;
+        private System.Windows.Forms.PictureBox pb_thumbnail;
+        private System.Windows.Forms.PictureBox pb_play;
+        private System.Windows.Forms.PictureBox pb_edit;
     }
 }
--- Learn CTS/GameCard.cs
+++ Learn CTS/GameCard.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Learn_CTS
@@ -10,10 +13,21 @@ namespace Learn_CTS
          * False otherwise
          */
         private bool defaultGame;
+        private readonly String img_path;
 
         public GameCard()
         {
             InitializeComponent();
+            this.img_path = AppDomain.CurrentDomain.BaseDirectory + "internal" + Path.DirectorySeparatorChar + "images" + Path.DirectorySeparatorChar;
+            PictureBox pb_play_parent = new PictureBox()
+            {
+                //Location = new Point(0, 0),
+                BackColor = Color.Transparent
+            };
+            pb_play.BackgroundImage = ChangeOpacity(Image.FromFile(img_path + "gamecard-play-btn-x128.png"), 0.5f);
+            pb_play.BackColor = Color.Transparent;
+            pb_edit.BackgroundImage = ChangeOpacity(Image.FromFile(img_path + "gamecard-edit-btn-x64.png"), 0.5f);
+            pb_edit.BackColor = Color.Transparent;
         }
 
         public String Title
@@ -25,7 +39,7 @@ namespace Learn_CTS
 
             set
             {
-                int char_space = 24; // Number of characters that can be seen in the label
+                int char_space = 14; // Number of characters that can be seen in the label
                 if (value.Length > char_space)
                 {
                     this.lbl_title.Text = value.Substring(0, char_space - 3) + "...";
@@ -34,6 +48,8 @@ namespace Learn_CTS
                 {
                     this.lbl_title.Text = value;
                 }
+                // We need to know the game title in order to retrieve the thumbnail.
+                Show_Thumbnail(value);
             }
         }
 
@@ -46,7 +62,7 @@ namespace Learn_CTS
 
             set
             {
-                int char_space = 64; // Number of characters that can be seen in the label
+                int char_space = 164; // Number of characters that can be seen in the label
                 if (value.Length > char_space)
                 {
                     this.lbl_description.Text = value.Substring(0, char_space - 3) + "...";
@@ -55,6 +71,30 @@ namespace Learn_CTS
                 {
                     this.lbl_description.Text = value;
                 }
+            }
+        }
+
+        public void Show_Thumbnail(String game)
+        {
+            String thumbnail_path = AppDomain.CurrentDomain.BaseDirectory + "games" + Path.DirectorySeparatorChar + game
+                + Path.DirectorySeparatorChar + "library" + Path.DirectorySeparatorChar + "images"
+                + Path.DirectorySeparatorChar + "others" + Path.DirectorySeparatorChar + "thumbnail.png";
+            try
+            {
+                pb_thumbnail.BackgroundImage = Image.FromFile(thumbnail_path);
+            }
+            catch (Exception ex)
+            {
+                pb_thumbnail.BackgroundImage = null;
+            }
+            finally
+            {
+                // Setting the parent of the icons in order to make them transparent.
+                pb_play.Parent = pb_thumbnail;
+                pb_play.Location = new Point(64 - pb_play.Width/2, 64 - pb_play.Height/2);
+                pb_edit.Parent = pb_thumbnail;
+                pb_edit.Location = new Point(128 - pb_edit.Width, 0);
+                pb_edit.BringToFront();
             }
         }
 
@@ -72,6 +112,83 @@ namespace Learn_CTS
             Form editor = new Editor(Title);
             editor.Show();
             this.Parent.Parent.Hide();
+        }
+
+        public static Bitmap ChangeOpacity(Image img, float opacityvalue)
+        {
+            Bitmap bmp = new Bitmap(img.Width, img.Height); // Determining Width and Height of Source Image
+            Graphics graphics = Graphics.FromImage(bmp);
+            ColorMatrix colormatrix = new ColorMatrix();
+            colormatrix.Matrix33 = opacityvalue;
+            ImageAttributes imgAttribute = new ImageAttributes();
+            imgAttribute.SetColorMatrix(colormatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+            graphics.DrawImage(img, new Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, imgAttribute);
+            graphics.Dispose();   // Releasing all resource used by graphics 
+            return bmp;
+        }
+
+        private void Pb_play_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Pb_edit_Click(object sender, EventArgs e)
+        {
+            Form editor = new Editor(Title);
+            editor.Show();
+            this.Parent.Parent.Hide();
+        }
+
+        private void Pb_play_MouseHover(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pb_play.BackgroundImage != null) pb_play.BackgroundImage.Dispose();
+                pb_play.BackgroundImage = ChangeOpacity(Image.FromFile(img_path + "gamecard-play-btn-x128.png"), 1);
+            }
+            catch (Exception ex)
+            {
+                pb_play.BackgroundImage = null;
+            }
+        }
+
+        private void Pb_play_MouseLeave(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pb_play.BackgroundImage != null) pb_play.BackgroundImage.Dispose();
+                pb_play.BackgroundImage = ChangeOpacity(Image.FromFile(img_path + "gamecard-play-btn-x128.png"), 0.7f);
+            }
+            catch (Exception ex)
+            {
+                pb_play.BackgroundImage = null;
+            }
+        }
+
+        private void Pb_edit_MouseHover(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pb_edit.BackgroundImage != null) pb_edit.BackgroundImage.Dispose();
+                pb_edit.BackgroundImage = ChangeOpacity(Image.FromFile(img_path + "gamecard-edit-btn-x64.png"), 1);
+            }
+            catch (Exception ex)
+            {
+                pb_edit.BackgroundImage = null;
+            }
+        }
+
+        private void Pb_edit_MouseLeave(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pb_edit.BackgroundImage != null) pb_edit.BackgroundImage.Dispose();
+                pb_edit.BackgroundImage = ChangeOpacity(Image.FromFile(img_path + "gamecard-edit-btn-x64.png"), 0.7f);
+            }
+            catch (Exception ex)
+            {
+                pb_edit.BackgroundImage = null;
+            }
         }
     }
 }
