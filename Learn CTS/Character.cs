ecad1e81dfc1d7ba3f081c538cab3c699c46b5cd
Orian STOFFEL <zarkosmc@gmail.com>
2019-11-09 18:28:49 +0100

    ADDED : The npcs can now have an objective and move toward it.

--- Learn CTS/Character.cs
+++ Learn CTS/Character.cs
@@ -16,6 +16,8 @@ namespace Learn_CTS
         private int id;
         private int c = 0;
         private int t = 0;
+        private int? obj_x;
+        private int? obj_y;
         private List<Image> animation_list_west = new List<Image>();
         private List<Image> animation_list_est = new List<Image>();
         private bool hasMoved = false;
@@ -146,6 +148,94 @@ namespace Learn_CTS
         public override string GetName()
         {
             return this.name;
+        }
+
+        /// <summary>
+        /// Set coordinates as objective
+        /// </summary>
+        /// <param name="x">The x coordinate of the objective.</param>
+        /// <param name="y">The y coordinate of the objective.</param>
+
+        public void GoTo(int x, int y)
+        {
+            this.obj_x = x - (this.GetX() + this.GetWidth() / 2);
+            this.obj_y = y - (this.GetY() + this.GetHeight());
+        }
+
+        /// <summary>
+        /// Get the x coordinate of the objective.
+        /// </summary>
+        /// <returns>The x coordinate that has to be reached.</returns>
+
+        public int GetObjX()
+        {
+            return (int)this.obj_x;
+        }
+
+        public void UpdateObjX(int x)
+        {
+            this.obj_x += x;
+        }
+
+        public void UpdateObjY(int y)
+        {
+            this.obj_y += y;
+        }
+
+        /// <summary>
+        /// Get the y coordinate of the objective.
+        /// </summary>
+        /// <returns>The y coordinate that has to be reached.</returns>
+
+        public int GetObjY()
+        {
+            return (int)this.obj_y;
+        }
+
+        /// <summary>
+        /// Check if the player has an objective.
+        /// </summary>
+        /// <returns>Return true if he has an objective, false otherwise.</returns>
+
+        public bool HasObjective()
+        {
+            return (this.obj_x != null && this.obj_y != null);
+        }
+
+        /// <summary>
+        /// Check if the player has reached his objective.
+        /// </summary>
+        /// <returns>True if the player has reached his objective.</returns>
+
+        public bool ReachedObjective()
+        {
+            return (
+                this.ReachedObjX() &&
+                this.ReachedObjY());
+        }
+
+        public bool ReachedObjX()
+        {
+            return (
+                this.obj_x < 8 &&
+                this.obj_x > -8);
+        }
+
+        public bool ReachedObjY()
+        {
+            return (
+                this.obj_y < 8 &&
+                this.obj_y > -8);
+        }
+
+        /// <summary>
+        /// Remove the player's objective.
+        /// </summary>
+
+        public void RemoveObjective()
+        {
+            this.obj_x = null;
+            this.obj_y = null;
         }
     }
 }
--- Learn CTS/Player.cs
+++ Learn CTS/Player.cs
@@ -9,10 +9,6 @@ namespace Learn_CTS
 {
     class Player : Character
     {
-        // Attributes
-
-        private int? obj_x;
-        private int? obj_y;
 
         /// <summary>
         /// Constructor of the player
@@ -22,94 +18,6 @@ namespace Learn_CTS
 
         public Player(String name, int x, int y) : base(0, name, "1", x, y)
         {
-        }
-
-        /// <summary>
-        /// Set coordinates as objective
-        /// </summary>
-        /// <param name="x">The x coordinate of the objective.</param>
-        /// <param name="y">The y coordinate of the objective.</param>
-
-        public void GoTo(int x, int y)
-        {
-            this.obj_x = x - (this.GetX() + this.GetWidth()/2);
-            this.obj_y = y - (this.GetY() + this.GetHeight());
-        }
-
-        /// <summary>
-        /// Get the x coordinate of the objective.
-        /// </summary>
-        /// <returns>The x coordinate that has to be reached.</returns>
-
-        public int GetObjX()
-        {
-            return (int)this.obj_x;
-        }
-
-        public void UpdateObjX(int x)
-        {
-            this.obj_x += x;
-        }
-
-        public void UpdateObjY(int y)
-        {
-            this.obj_y += y;
-        }
-
-        /// <summary>
-        /// Get the y coordinate of the objective.
-        /// </summary>
-        /// <returns>The y coordinate that has to be reached.</returns>
-
-        public int GetObjY()
-        {
-            return (int)this.obj_y;
-        }
-
-        /// <summary>
-        /// Check if the player has an objective.
-        /// </summary>
-        /// <returns>Return true if he has an objective, false otherwise.</returns>
-
-        public bool HasObjective()
-        {
-            return (this.obj_x != null && this.obj_y != null);
-        }
-
-        /// <summary>
-        /// Check if the player has reached his objective.
-        /// </summary>
-        /// <returns>True if the player has reached his objective.</returns>
-
-        public bool ReachedObjective()
-        {
-            return (
-                this.ReachedObjX() &&
-                this.ReachedObjY());
-        }
-
-        public bool ReachedObjX()
-        {
-            return (
-                this.obj_x < 8 &&
-                this.obj_x > -8);
-        }
-
-        public bool ReachedObjY()
-        {
-            return (
-                this.obj_y < 8 &&
-                this.obj_y > -8);
-        }
-
-        /// <summary>
-        /// Remove the player's objective.
-        /// </summary>
-
-        public void RemoveObjective()
-        {
-            this.obj_x = null;
-            this.obj_y = null;
         }
     }
 }
