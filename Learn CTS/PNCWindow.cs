b09fe0f4f91dfd19c4f9192315c3b31df68ab5c4
Louis Allemann <36743745+jeanlouison@users.noreply.github.com>
2019-11-23 19:11:55 +0100

    UPDATED DirectoryCopy in Tools.cs. DELETED games copying, for now. UPDATED PNC ItemViewer

--- Learn CTS/PNCWindow.cs
+++ Learn CTS/PNCWindow.cs
@@ -31,7 +31,7 @@ namespace Learn_CTS
             this.game_path = this.game_path = System.AppDomain.CurrentDomain.BaseDirectory + "games" + Path.DirectorySeparatorChar + game + Path.DirectorySeparatorChar;
             this.Text = game;
             item_manager = ItemManager.GetInstance();
-            DoubleBuffered = false;
+            DoubleBuffered = true;
             InitializeComponent();
         }
 
@@ -105,75 +105,13 @@ namespace Learn_CTS
 
         private void HighlightItem(Item item)
         {
-            // Panel used to darken the background elements
-            Panel hg_pnl_color = new Panel()
+            ItemViewer iv = new ItemViewer(item.GetID())
             {
-                Location = new Point(0, 0),
-                Width = this.Width,
-                Height = this.Height,
-                Name = "hg_pnl_color",
-                BackColor = Color.FromArgb(80, 0, 0, 0)
+                Name = "iv",
+                Width = this.Width
             };
-            hg_pnl_color.MouseDown += new MouseEventHandler(DisableHighlight);
-
-            // PictureBox to preview the item
-            PictureBox hg_pb_item = new PictureBox()
-            {
-                BackgroundImage = item.GetImage(),
-                Parent = hg_pnl_color,
-                Name = "hg_pb_item",
-                Height = item.GetImage().Height,
-                Width = item.GetImage().Width
-            };
-            hg_pb_item.Location = new Point((this.Width / 2) - (hg_pb_item.Width * 2), (this.Height / 2) - (hg_pb_item.Height / 2));
-
-            // Label to view the item description
-            Label hg_lbl_desc = new Label()
-            {
-                Text = item.GetDescription(),
-                BackColor = Color.Transparent,
-                Name = "hg_lbl_desc",
-                Parent = hg_pnl_color,
-                AutoSize = true,
-                MaximumSize = new Size(this.Width - ((this.Width / 2) + (this.Width / 20)), this.Height - hg_pb_item.Location.Y),
-            };
-            hg_lbl_desc.Location = new Point((this.Width / 2) + (this.Width / 20), (this.Height / 2) + (hg_lbl_desc.Height / 2));
-            hg_lbl_desc.MouseDown += new MouseEventHandler(DisableHighlight);
-
-            this.Controls.AddRange(new Control[] { hg_pb_item, hg_lbl_desc, hg_pnl_color });
-
-        }
-
-        private void DisableHighlight(object sender, MouseEventArgs e)
-        {
-            foreach (Control c in this.Controls)
-            {
-                switch (c.Name)
-                {
-                    case "hg_pnl_color":
-                        c.Dispose();
-                        break;
-                    case "hg_pb_item":
-                        c.Dispose();
-                        break;
-                    case "hg_lbl_desc":
-                        c.Dispose();
-                        break;
-                }
-            }
-        }
-
-        public static Bitmap ChangeOpacity(Image img, float opacityvalue)
-        {
-            Bitmap bmp = new Bitmap(img.Width, img.Height); // Determining Width and Height of Source Image
-            Graphics graphics = Graphics.FromImage(bmp);
-            ColorMatrix colormatrix = new ColorMatrix();
-            colormatrix.Matrix33 = opacityvalue;
-            ImageAttributes imgAttribute = new ImageAttributes();
-            imgAttribute.SetColorMatrix(colormatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
-            graphics.DrawImage(img, new Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, imgAttribute);
-            graphics.Dispose();   // Releasing all resource used by graphics 
-            return bmp;
+            iv.Location = new Point(0, this.Height - 165);
+            this.Controls.Add(iv);
         }
 
         private void PNCWindow_SizeChanged(object sender, EventArgs e)
@@ -182,15 +120,9 @@ namespace Learn_CTS
             {
                 switch (c.Name)
                 {
-                    case "hg_pnl_color":
+                    case "iv":
                         c.Width = this.Width;
-                        c.Height = this.Height;
-                        break;
-                    case "hg_pb_item":
-                        c.Location = new Point((this.Width / 2) - (c.Width * 2), (this.Height / 2) - (c.Height / 2));
-                        break;
-                    case "hg_lbl_desc":
-                        c.MaximumSize = new Size(this.Width - ((this.Width / 2) + (this.Width / 20)), this.Height - Controls.Find("hg_pb_item", true)[0].Location.Y);
+                        c.Location = new Point(0, this.Height - 165);
                         break;
                 }
             }
